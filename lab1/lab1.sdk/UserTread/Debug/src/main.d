8381f07c7b034187ee26a8ecd2725fe643c1dd89
David <entvex@gmail.com>
2018-10-24 15:12:59 +0200

    PSO code

---
+++ lab1/lab1.sdk/UserTread/Debug/src/main.d
@@ -0,0 +1,39 @@
+src/main.o src/main.o: ../src/main.cpp ../src/UserThread.h \
+ C:\Users\Entvex\lab1\lab1.sdk\UserTread\src\os/Thread.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/projdefs.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/portable.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/portmacro.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/task.h \
+ ../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/list.h
+
+../src/UserThread.h:
+
+C:\Users\Entvex\lab1\lab1.sdk\UserTread\src\os/Thread.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/xparameters.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/projdefs.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/portable.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/portmacro.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/task.h:
+
+../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include/list.h:
---
+++ lab1/lab1.sdk/UserTread/Debug/src/os/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/os/Mutex.cpp \
+../src/os/Semaphore.cpp \
+../src/os/Thread.cpp 
+
+OBJS += \
+./src/os/Mutex.o \
+./src/os/Semaphore.o \
+./src/os/Thread.o 
+
+CPP_DEPS += \
+./src/os/Mutex.d \
+./src/os/Semaphore.d \
+./src/os/Thread.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/os/%.o: ../src/os/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM v7 g++ compiler'
+	arm-none-eabi-g++ -Wall -O0 -g3 -I"C:\Users\Entvex\lab1\lab1.sdk\UserTread\src\os" -c -fmessage-length=0 -MT"$@" -mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=hard -I../../freertos901_xilinx_bsp_0/ps7_cortexa9_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
