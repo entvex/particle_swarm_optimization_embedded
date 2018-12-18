b45722fef6b7c213a20047234486ab5aebbecef1
Bagger <black_volt@hotmail.com>
2018-12-18 11:48:07 +0100

    hls update

--- hls/psos/psos/script.tcl
+++ hls/psos/psos/script.tcl
@@ -3,17 +3,15 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2017 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project particles.prj
-set_top particlemaster
-add_files particles.prj/Particles.h
-add_files particles.prj/Particles.cpp
-add_files particles.prj/ParticleMaster.h
-add_files particles.prj/ParticleMaster.cpp
-add_files -tb particles.prj/main.cpp
-open_solution "solution1"
+open_project psos
+set_top psosc
+add_files psos/psos.cpp
+add_files psos/psos.h
+add_files -tb psos/main.cpp
+open_solution "psos"
 set_part {xc7z010clg400-1} -tool vivado
 create_clock -period 10 -name default
-#source "./particles.prj/solution1/directives.tcl"
+#source "./psos/psos/directives.tcl"
 csim_design
 csynth_design
 cosim_design
---
+++ hls/psos/vivado_hls.app
@@ -0,0 +1,16 @@
+<project xmlns="com.autoesl.autopilot.project" top="psosc" name="psos" projectType="SystemC">
+    <includePaths/>
+    <libraryPaths/>
+    <Simulation>
+        <SimFlow name="csim" csimMode="0" lastCsimMode="0"/>
+    </Simulation>
+    <solutions xmlns="">
+        <solution name="psos" status="active"/>
+    </solutions>
+    <files xmlns="">
+        <file name="../main.cpp" sc="1" tb="1" cflags=" "/>
+        <file name="psos/psos.h" sc="1" tb="false" cflags=""/>
+        <file name="psos/psos.cpp" sc="1" tb="false" cflags=""/>
+    </files>
+</project>
+
